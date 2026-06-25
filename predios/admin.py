59abfe9cfdc1d26768a6d597081f29f9f80714bf
MatheusBC <matheusbevilacqua4@gmail.com>
2026-06-25 19:49:05 -0300

    Delete predios directory

--- predios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Predio
-
-
-@admin.register(Predio)
-class PredioAdmin(admin.ModelAdmin):
-	list_display = ('id', 'endereco')
-	search_fields = ('endereco',)
--- predios/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrediosConfig(AppConfig):
-    name = 'predios'
